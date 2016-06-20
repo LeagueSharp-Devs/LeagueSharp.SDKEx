b53720f5526a3237d06bdc7fc53f9443c3391fcb
Alex <thinkinginbinary@bk.ru>
2016-06-20 21:58:43 +0300

    new Spell override to use L#.Data (will fail to compile until CI builds .Data changes)

--- Core/Wrappers/Spells/Database/SpellDatabase.cs
+++ Core/Wrappers/Spells/Database/SpellDatabase.cs
@@ -23,6 +23,8 @@ namespace LeagueSharp.SDK
 
     using LeagueSharp.Data;
     using LeagueSharp.Data.DataTypes;
+    using LeagueSharp.Data.Enumerations;
+    using LeagueSharp.SDK.Enumerations;
 
     /// <summary>
     ///     The spell database.
@@ -103,6 +105,103 @@ namespace LeagueSharp.SDK
                     .FirstOrDefault(spellData => objectName.Contains(spellData.SourceObjectName));
         }
 
+        public static SpellDatabaseEntry GetBySpellSlot(SpellSlot slot, string championName = "undefined")
+        {
+            var actualChampionName = championName.Equals("undefined")
+                                         ? ObjectManager.Player.CharData.BaseSkinName
+                                         : championName;
+            return
+                Spells.FirstOrDefault(
+                    spellData => spellData.ChampionName == actualChampionName && spellData.Slot == slot);
+        }
+
+        public static IEnumerable<SpellDatabaseEntry> GetAllSpellsOnSpellSlot(
+            SpellSlot slot,
+            string championName = "undefined")
+        {
+            var actualChampionName = championName.Equals("undefined")
+                                         ? ObjectManager.Player.CharData.BaseSkinName
+                                         : championName;
+            return Spells.Where(spellData => spellData.ChampionName == actualChampionName && spellData.Slot == slot);
+        }
+
+        public static Spell MakeSpell(this SpellSlot slot, string championName = "undefined")
+        {
+            var spellData = GetBySpellSlot(slot, championName);
+            // Charged Spell:
+            if (spellData.ChargedSpellName != "")
+            {
+                return new Spell
+                           {
+                               ChargedBuffName = spellData.ChargedBuffName, ChargedMaxRange = spellData.ChargedMaxRange,
+                               ChargedMinRange = spellData.ChargedMinRange, ChargedSpellName = spellData.ChargedSpellName,
+                               ChargeDuration = spellData.ChargeDuration, Delay = spellData.Delay, Range = spellData.Range,
+                               Width =
+                                   spellData.Radius > 0 && spellData.Radius < 30000
+                                       ? spellData.Radius
+                                       : ((spellData.Width > 0 && spellData.Width < 30000) ? spellData.Width : 30000),
+                               Collision =
+                                   (spellData.CollisionObjects != null
+                                    && spellData.CollisionObjects.Any(obj => obj == CollisionableObjects.Minions)),
+                               Speed = spellData.MissileSpeed, IsChargedSpell = true,
+                               Type = GetSkillshotTypeFromSpellType(spellData.SpellType)
+                           };
+            }
+            // Skillshot:
+            if (spellData.CastType.Any(type => type == CastType.Position || type == CastType.Direction))
+            {
+                return new Spell
+                {
+                    Delay = spellData.Delay,
+                    Range = spellData.Range,
+                    Width =
+                                   spellData.Radius > 0 && spellData.Radius < 30000
+                                       ? spellData.Radius
+                                       : ((spellData.Width > 0 && spellData.Width < 30000) ? spellData.Width : 30000),
+                    Collision =
+                                   (spellData.CollisionObjects != null
+                                    && spellData.CollisionObjects.Any(obj => obj == CollisionableObjects.Minions)),
+                    Speed = spellData.MissileSpeed,
+                    IsSkillshot = true,
+                    Type = GetSkillshotTypeFromSpellType(spellData.SpellType)
+                };
+            }
+            // Targeted:
+            return new Spell { Range = spellData.Range, Delay = spellData.Delay, Speed = spellData.MissileSpeed };
+        }
+
+        /// <summary>
+        /// Returns the SDK alternative to the LeagueSharp.Data SpellType.
+        /// </summary>
+        /// <param name="spellType">The LeagueSharp.Data SpellType</param>
+        /// <returns>The SDK SpellType</returns>
+        public static SkillshotType GetSkillshotTypeFromSpellType(LeagueSharp.Data.Enumerations.SpellType spellType)
+        {
+            switch (spellType)
+            {
+                case SpellType.SkillshotArc:
+                    return SkillshotType.SkillshotCone;
+                case SpellType.SkillshotCone:
+                    return SkillshotType.SkillshotCone;
+                case SpellType.SkillshotCircle:
+                return SkillshotType.SkillshotCircle;
+                case SpellType.SkillshotLine:
+                return SkillshotType.SkillshotLine;
+                case SpellType.SkillshotMissileArc:
+                return SkillshotType.SkillshotCone;
+                case SpellType.Position:
+                return SkillshotType.SkillshotLine;
+                case SpellType.SkillshotMissileCircle:
+                return SkillshotType.SkillshotCircle;
+                case SpellType.SkillshotMissileLine:
+                return SkillshotType.SkillshotLine;
+                case SpellType.SkillshotMissileCone:
+                return SkillshotType.SkillshotCircle;
+                case SpellType.SkillshotRing:
+                return SkillshotType.SkillshotCircle;
+            }
+            return SkillshotType.SkillshotLine;
+        }
         #endregion
     }
 }
